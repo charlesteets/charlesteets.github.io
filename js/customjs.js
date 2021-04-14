082a34dc40ab121cf961dd9c67c7b6bb14af4c40
charlesteets <71031886+charlesteets@users.noreply.github.com>
2021-04-14 13:55:51 -0400

    Update customjs.js

--- js/customjs.js
+++ js/customjs.js
@@ -9,7 +9,7 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
 if (!isMobile) {
   var elements = document.getElementsByClassName("projectDetailsLink");
   for(var i=0; i<elements.length; i++) {
-    elements[i].setAttribute("href","");
+    elements[i].setAttribute("href","#");
   }
 }
 
